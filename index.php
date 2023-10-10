8d3b665386f76e1de7923edd4c0870f26f018b17
Bram Huiskes <bram.huiskes@student.nhlstenden.com>
2023-10-10 10:23:51 +0200

    Added File & Folder Structure and added basic files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sunny | Homepage</title>
+    <link href="assets/css/includes/header.css" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <?php include "./includes/header.php" ?>
+
+</body>
+</html>
