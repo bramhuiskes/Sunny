ee397b86dd36c31515571e716468d3b34c471001
jesseNHLStenden <jesse.van.der.voet@student.nhlstenden.com>
2023-10-13 18:45:17 +0200

    test-product-page.css | fonts now work through @font-face, product-page-filter-handler.php | Added filters that are needed and added a layer of protection.

--- product-page-filter-handler.php
+++ product-page-filter-handler.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 function generateProductPage()
 {
     $productArray = getFilterResult();
@@ -10,10 +8,9 @@ function generateProductPage()
     {
         echo"<div class=\"temp-product\"><img src=" . $value['ProductThumbnail'] . " alt=\"Intel sok\">";
         echo"<p>" . $value['ProductName'] . "</p>";
-        echo"<p>" . $value['ProductProperties']['Price'] . "</p></div>";
+        echo"<p>" . $value['ProductProperties']['Price'] . "</p>";
+        echo"<input type=\"hidden\" value=" . $key ."></div>";
     }
-
-    return $productArray;
 }
 
 function getFilterResult()
@@ -21,18 +18,48 @@ function getFilterResult()
     $json_file = file_get_contents('./assets/json/products.json');
     $products = json_decode($json_file, true);
 
-    if(!isset($_GET['sock']))
+    if(!isset($_GET['colorFilter']) && !isset($_GET['collection']))
     {
         //returns all socks
         return $products;
     }
-    elseif(isset($_GET['sock']))
+    else
     {
-        //returns with queried paramaters
-        return;
+        $color = null;
+        $collection = null;
+
+        if(isset($_GET['colorFilter']) && $_GET['colorFilter'] != '')
+            $color = filter_var($_GET['colorFilter'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+        if(isset($_GET['collection']) && $_GET['collection'] != '')
+            $collection = filter_var($_GET['collection'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+        $queriedProducts = array();
+
+        if($color != null && $collection != null){
+            foreach($products as $key=>$value){
+                if($value['ProductProperties']['Color'] == $color && $value['ProductProperties']['Collection'] == $collection){
+                    array_push($queriedProducts, $value);
+                }
+            }
+        }
+        elseif($color != null){
+            foreach($products as $key=>$value){
+                if($value['ProductProperties']['Color'] == $color){
+                    array_push($queriedProducts, $value);
+                }
+            }
+        }
+        elseif($collection != null){
+            foreach($products as $key=>$value){
+                if($value['ProductProperties']['Collection'] == $collection){
+                    array_push($queriedProducts, $value);
+                }
+            }
+        }
+
+        //returns with queried parameters
+        return $queriedProducts;
     }
 }
-
-
-
 ?>
