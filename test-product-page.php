1b4f8e200b65de8b0272f67b806497d4abe5873c
JesseNHLStenden <jesse.van.der.voet@student.nhlstenden.com>
2023-10-13 12:37:54 +0200

    test-product-page.css | Added stylesheet with minor css, product-page-filter-handler.php | added backend functionality so it add html to the page, test-product-page.php | now displays a grid with the product

--- test-product-page.php
+++ test-product-page.php
@@ -3,14 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/test-product-page.css" type="text/css">
+    <title>Sunny | Test page</title>
 </head>
 <body>
+    <div class="container">
     <?php
     //Experimental php page, Do not touch or use any code!
     include('product-page-filter-handler.php');
     
     generateProductPage();
     ?>
+    </div>
 </body>
 </html>
