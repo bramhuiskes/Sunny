7111a43033c4cf09b8649cca724c7e1b8a7ddcb2
Gideon Dijkhuis <gideon.dijkhuis@student.nhlstenden.com>
2023-10-13 12:50:58 +0200

    PHP code added for calculating prices with discount, and css added for better looks on the shopping-cart page

--- shopping-cart.php
+++ shopping-cart.php
@@ -1,6 +1,6 @@
 <?php
 $cookie_name = "shopping-cart-content";
-$cookie_array = array("101","102","101");
+$cookie_array = array("101" => 4,"102" => 5,"201" => 6);
 
 setcookie($cookie_name,json_encode($cookie_array), time() + (86400 * 30), "/");
 ?>
@@ -18,32 +18,7 @@ setcookie($cookie_name,json_encode($cookie_array), time() + (86400 * 30), "/");
 <body>
     <?php include "./includes/header.php" ?>
     <div>
-        <!--<div class="shopping-cart empty">
-            <h1>
-                Your cart is currently empty
-            </h1>
-            <a href="#" class="btn">
-                RETURN TO SHOP
-            </a>
-        </div>-->
-        <div class="shopping-cart filled">
-            <div class="item">
-                <div class="detail">
-                    <div class="count">
-                        <p>1</p>
-                    </div>
-                    <img src="./assets/img/products/sokken-stripes-blue.png" alt="intel Sok!">
-                    <h2>Blue Stripe socks</h2>
-                </div>  
-                <div class="btncontainer">
-                    <p>Amount: </p>
-                    <input type="number" class="item-count" max="9" min="1" value="1" name="item-count">
-                    <a href="" class="delete">
-                        <img src="./assets/img/svg/delete.svg" alt="delete">
-                    </a>
-                </div>
-            </div>
-        </div>
+        
         <?php
             if(isset($_COOKIE[$cookie_name])){
                 $cookie_array = json_decode($_COOKIE[$cookie_name]);
@@ -52,13 +27,68 @@ setcookie($cookie_name,json_encode($cookie_array), time() + (86400 * 30), "/");
 
                 $products = json_decode($json_file, true);
 
-                var_dump($products);
+                $i = 0;
 
-                foreach($cookie_array as $id){
-                    $productDetails = $products[$id];
+                $price = 0.0;
 
-                    print $productDetails['ProductName'];
+                echo "<div class=\"shopping-cart filled\">";
+
+                foreach($cookie_array as $key=>$value){
+                    $i++;
+
+                    $productDetails = $products[$key];
+
+                    $productName =  $productDetails['ProductName'];
+                    $productImg = $productDetails['ProductThumbnail'];
+
+                    $amount = $value;
+
+                    $productProp = $productDetails['ProductProperties'];
+
+                    $price += 
+                    ($productProp['Price']*$amount);
+
+                    echo "
+                    <div class=\"item\">
+                        <div class=\"detail\">
+                            <div class=\"count\">
+                                <p>{$i}</p>
+                            </div>
+                            <img src=\"{$productImg}\" alt=\"intel Sok!\">
+                            <h2>{$productName}</h2>
+                        </div>  
+                        <div class=\"btncontainer\">
+                            <p>Amount: </p>
+                            <input type=\"number\" class=\"item-count\" max=\"9\" min=\"1\" value=\"{$amount}\" name=\"item-count\">
+                            <a href=\"\" class=\"delete\">
+                                <img src=\"./assets/img/svg/delete.svg\" alt=\"delete\">
+                            </a>
+                        </div>
+                    </div>
+                    ";
                 }
+                echo "</div>";
+
+                echo "<p style=\"text-decoration: line-through;\">"."€".  $price . "</p>";
+
+                $discount = $price/11.97;
+
+                $discount = ((((int)round($discount))+1)*1.98);
+
+                $price -= $discount;
+                echo "€".$price;
+                $arr_discount = explode(".",$discount);
+                echo "<br>Discount: €". $arr_discount[0].".".substr(strval($arr_discount[1]),0,2);
+            } else {
+                echo "
+                <div class=\"shopping-cart empty\">
+                    <h1>
+                        Your cart is currently empty
+                    </h1>
+                    <a href=\"#\" class=\"btn\">
+                        RETURN TO SHOP
+                    </a>
+                </div>";
             }
             
         ?>
