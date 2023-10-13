821c1be46b4fc33ef38890fbc2e352b91f77cd2b
Bram Huiskes <bram.huiskes@student.nhlstenden.com>
2023-10-13 12:17:48 +0200

    Made the homepage

--- index.php
+++ index.php
@@ -5,10 +5,321 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sunny | Homepage</title>
     <link href="assets/css/includes/header.css" rel="stylesheet" />
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/homepage.css">
 </head>
 <body>
     <?php include "./includes/header.php" ?>
+    <div class="div">
+  <div class="div-2">
+    <img
+      loading="lazy"
+      src="assets/img/logo/logo.png"
+      class="img"
+    />
+    <div class="div-3">
+      <div class="div-4">
+        <div class="div-5">
+          <div class="div-6">
+            <div class="div-7"></div>
+            <div class="div-8">Home</div>
+          </div>
+          <div class="div-9">NEW IN</div>
+          <div class="div-10">SHOP ALL</div>
+          <div class="div-11">COLLECTIONS</div>
+          <div class="div-12">CONTACT US</div>
+        </div>
+        <div class="div-13" >
+          <img
+            loading="lazy"
+            src="assets/img/svg/shopping-cart.svg"
+            class="img-2"
+            id="cart-button"
+            onclick="dropdown.style.display = 'block';"
+          />
+          <div class="div-14">0</div>
+
+        <div class="cart-container">
+            <div id="cart-dropdown" class="cart-dropdown">
+                <ul id="cart-items"></ul>
+                <div class="cart-total">
+                    <span>Total:</span>
+                    <span id="cart-total"></span>
+                </div>
+                <button id="view-cart-button">View Cart</button>
+            </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="div-15">
+    <div class="div-16">FEEL GOOD</div>
+    <div class="div-17">
+      <div class="div-18">
+        <div class="column"><div class="div-19">SUNNY</div></div>
+        <div class="column-2">
+          <div class="div-20">
+            <img
+              loading="lazy"
+              src="assets/img/illustrations/orange.png"
+              class="img-3"
+            />
+            <img
+              loading="lazy"
+              src="assets/img/illustrations/yellow.png"
+              class="img-4"
+            />
+          </div>
+        </div>
+        <section style="margin-bottom: 50px; margin-left: 50px;">
+        <div style="width: 450px; height: 142px; position: relative; border: 1px #F15A39 solid;">
+  <div style="width: 190px; height: 18px; left: 19px; top: 100px; position: absolute; color: #F15A39; font-size: 16px; font-family: Nexa Bold; font-weight: 700; text-transform: uppercase; line-height: 18px; word-wrap: break-word">view all products</div>
+  <div style="width: 231px; height: 80px; left: 1px; top: 1px; position: absolute; border-right: 1px #F15A39 solid; border-bottom: 1px #F15A39 solid"></div>
+  <div style="width: -webkit-fill-available; height: 80px; left: 232px; top: 1px; position: absolute; border-bottom: 1px #F15A39 solid"></div>
+  <div style="width: 160px; height: 18px; left: 19px; top: 32px; position: absolute; color: #F15A39; font-size: 16px; font-family: Nexa Bold; font-weight: 700; text-transform: uppercase; line-height: 18px; word-wrap: break-word">NEW COLLECTION</div>
+  <div style=" height: 62px; left: 250px; right: 10px; top: 15%; position: absolute; color: #F15A39; font-size: 16px; font-family: Nexa Bold; font-weight: 700; text-transform: uppercase; line-height: 18px; word-wrap: break-word; align-items: center; ">READ OUR VISION FOR A BETTER FUTURE</div>
+  <div style="width: 56px; height: 58px; left: 615px; top: 137px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0">
+    <div style="width: 18.67px; height: 29px; left: 18.67px; top: 14.50px; position: absolute; border-radius: 2px; border: 1.50px #F15A39 solid"></div>
+  </div>
+  <div style="width: 56px; height: 58px; left: 224px; top: 69px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0">
+  </div>
+  <div style="width: 56px; height: 58px; left: 615px; top: 65px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0">
+    <div style="width: 18.67px; height: 29px; left: 18.67px; top: 14.50px; position: absolute; border-radius: 2px; border: 1.50px #F15A39 solid"></div>
+  </div>
+</div>
+        </div>
+      </div>
+    </div></section>
+  </div>
+  <div class="div-28">
+    <div class="div-29">
+      <div class="div-30 scroll-container">
+        <div class="div-31 scroll-item"></div>
+        <div class="div-32 scroll-item">WORLDWIDE SHIPPING</div>
+        <div class="div-33 scroll-item"></div>
+        <div class="div-34 scroll-item">WORLDWIDE SHIPPING</div>
+        <div class="div-35 scroll-item"></div>
+        <div class="div-36 scroll-item">WORLDWIDE SHIPPING</div>
+        <div class="div-37 scroll-item"></div>
+        <div class="div-38 scroll-item">WORLDWIDE SHIPPING</div>
+        <div class="div-39 scroll-item"></div>
+        <div class="div-40 scroll-item">WORLDWIDE SHIPPING</div>
+        <div class="div-41 scroll-item"></div>
+        <div class="div-42 scroll-item">WORLDWIDE SHIPPING</div>
+        <div class="div-43 scroll-item"></div>
+        <div class="div-44 scroll-item">WORLDWIDE SHIPPING</div>
+        <div class="div-45 scroll-item"></div>
+        <div class="div-46 scroll-item">WORLDWIDE SHIPPING</div>
+        <div class="div-47 scroll-item"></div>
+        <div class="div-48 scroll-item">WORLDWIDE SHIPPING</div>
+        <div class="div-49 scroll-item"></div>
+        <div class="div-50 scroll-item">WORLDWIDE SHIPPING</div>
+        <div class="div-51 scroll-item"></div>
+        <div class="div-52 scroll-item">WORLDWIDE SHIPPING</div>
+      </div>
+    </div>
+    <div class="div-53">
+      <div class="div-54">
+      <div class="column-9">
+          <div class="div-62">
+            <div class="div-63">
+              <div class="div-64">
+                <div class="column-10">
+                  <img
+                    loading="lazy"
+                    src="assets/img/catalogus/red.jpg"
+                    class="img-9"
+                  />
+                </div>
+                <div class="column-11">
+                  <div class="div-65">
+                    <div class="div-66">
+                      Sale
+                      <br />
+                      Up to 50% off
+                    </div>
+                    <div class="div-67"><div class="div-68">SHOP NOW</div></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column-9">
+          <div class="div-62">
+            <div class="div-63">
+              <div class="div-64">
+                <div class="column-10">
+                <img
+                    loading="lazy"
+                    src="assets/img/catalogus/red.jpg"
+                    class="img-9"
+                  />
+                </div>
+                <div class="column-11">
+                  <div class="div-65">
+                    <div class="div-66">
+                      Sale
+                      <br />
+                      Up to 50% off
+                    </div>
+                    <div class="div-67"><div class="div-68">SHOP NOW</div></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="div-73">
+    <div class="div-74">
+      <div class="div-75">Our Values</div>
+      <div class="div-76">
+        <div class="div-77">
+          <div class="column-12">
+            <div class="div-78">
+              <div class="div-79">
+                <div class="div-80">
+                  <div class="div-81">Positive</div>
+                  <img
+                    loading="lazy"
+                    src="assets/img/svg/arrow.svg"
+                    class="img-11"
+                  />
+                </div>
+                <div class="div-82">
+                  Positivity is part of our DNA. Sunny likes to look on the
+                  bright side. Every problem is an oppurtunity to come up with a
+                  solution.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column-13">
+            <div class="div-83">
+              <div class="div-84">
+                <div class="div-85">
+                  <div class="div-86">Colorful</div>
+                  <img
+                    loading="lazy"
+                    src="assets/img/svg/arrow.svg"
+                    class="img-11"
+                  />
+                </div>
+                <div class="div-87">
+                  We like to add some color to one of the the most everyday
+                  moments of life: putting on your socks. We add literal color
+                  by designing simple basic white socks a pop of color.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column-14">
+            <div class="div-88">
+              <div class="div-89">
+                <div class="div-90">
+                  <div class="div-91">Beneficial</div>
+                  <img
+                    loading="lazy"
+                    src="assets/img/svg/arrow.svg"
+                    class="img-11"
+                  />
+                </div>
+                <div class="div-92">
+                  We like win-win situations. When we produce and sell our socks
+                  it is beneficial for everyone involved. The sells person has a
+                  nice job. The raw materi- als are gained in such a way it does
+                  not harm the earth. The production is done with the
+                  enivironment in mind and with an honest waige for the people
+                  working in the factories. The shops that sell our sock make a
+                  fair provit. And last but not least: the costumer gets high
+                  quality socks for a fair price.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="div-93">
+      <div class="div-94">
+        <div class="div-95">
+          <img
+            loading="lazy"
+            src="assets/img/svg/shipping.svg"
+            class="img-14"
+          />
+          <div class="div-96">INTERNATIONAL SHIPPING</div>
+        </div>
+        <div class="div-97">
+          <img
+            loading="lazy"
+            src="assets/img/svg/warranty.svg"
+            class="img-15"
+          />
+          <div class="div-98">6 month warranty</div>
+        </div>
+        <div class="div-99">
+          <img
+            loading="lazy"
+            src="assets/img/svg/safe.svg"
+            class="img-16"
+          />
+          <div class="div-100">safe shopping</div>
+        </div>
+        <div class="div-101">
+          <img
+            loading="lazy"
+            src="assets/img/svg/return.svg"
+            class="img-17"
+          />
+          <div class="div-102">FREE RETURNS</div>
+        </div>
+      </div>
+    </div>
+    <div class="div-103">
+      <div class="div-104">
+        <div class="div-105">
+          <div class="div-106">
+            <div class="column-15">
+              <div class="div-107">
+                <img
+                  loading="lazy"
+                  src="assets/img/svg/logo-black.svg"
+                  class="img-18"
+                />
+                <div class="div-108">The bright side of socks</div>
+              </div>
+            </div>
+            <div class="column-16">
+              <div class="div-109">
+                <div class="div-110">
+                  <div class="div-111">Instagram</div>
+                  <div class="div-112">FACEBOOK</div>
+                  <div class="div-113">Twitter</div>
+                </div>
+              </div>
+            </div>
+            <div class="column-17">
+              <div class="div-114">
+                <div class="div-115">
+                  <div class="div-116">© 2023</div>
+                  <div class="div-117">SUNNY</div>
+                  <div class="div-118">,</div>
+                </div>
+                <div class="div-119">All Rights Reserved</div>
+              </div>
+            </div>
+          </div> 
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 </body>
+<script src="assets/js/homepage.js"></script>
+
 </html>
