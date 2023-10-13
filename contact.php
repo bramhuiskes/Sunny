1244a1d25922dfca127b5ffa48ec93407627726c
LucasLubbers <lucas.lubbers@student.nhlstenden.com>
2023-10-13 10:28:56 +0200

    Added more of the product images, made contact.php and contact.css which is the first part of the contact page.

---
+++ contact.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/contact.css">
+    <title>Sunny | Contact us</title>
+</head>
+<body>
+    <?php include "./includes/header.php" ?>
+
+    <div class="contact-container">
+        <div class="contact-container-left-side">
+            <p class="contact-hello">Say Hello To Us</p>
+            <p class="contact-info">Has ut fabulas detraxit, maluisset mel id. Eam ad option albucius cu, exerci scripta, torquatos at mea. Duis veniam tantas vim ut mei unum. <br><br>4976 Oakway Lane <br>Los Angeles <br><br>123-456-7899 <br><br>Or send us a message</p> 
+            <img src="assets/img/contact-us-arrow.png" alt="contact-us-arrow.png">
+        </div>
+        <div class="contact-container-right-side">
+            <img src="assets/img/products/Sunny-socks-red.jpg" alt="Sunny-socks-red.jpg" class="sunny-socks-red">
+            <img src="assets/img/products/Sunny-socks-uni-green-1.png" alt="Sunny-socks-uni-green-1.png" class="sunny-socks-uni-green-1">
+        </div>
+    </div>
+</body>
+</html>
