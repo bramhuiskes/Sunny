a8d50c8188fd1eecd7276e44515576b36b9dfd75
LucasLubbers <lucas.lubbers@student.nhlstenden.com>
2023-11-03 09:16:14 +0100

    Made the dutch product page up to date.

--- SunnyNL/includes/product-page-filter-handler.php
+++ SunnyNL/includes/product-page-filter-handler.php
@@ -1,5 +1,6 @@
 <?php
 
+//function just generates a random int and generates a product field of 4
 function generateRandomProducts(){
     $productArray = getFilterResult();
     $rndInt = rand(0,6);
@@ -12,24 +13,38 @@ function generateRandomProducts(){
     }
 }
 
+//function just adds buttons when more than 6 products are generated
+function generatePaging(){
+    if(count(getFilterResult()) > 6){
+        echo "        <form method=\"get\" class=\"paging-nav\">
+        <input type=\"submit\" name=\"page\" value=\"1\" class=\"page-button\">
+        <input type=\"submit\" name=\"page\" value=\"2\" class=\"page-button\">
+    </form>";
+    }
+}
+
 function generateProductPage()
 {
+    //fetch the data from function bellow
     $productArray = getFilterResult();
 
     $indexOffset = 0;
 
+    //checks whether the page is equal to ' ' or 1
+    //supplies offset depending on page
     if(isset($_GET['page'])){
         if($_GET['page'] == 2){
             $indexOffset = 6;
         }
     }
     
-        foreach (array_slice($productArray, $indexOffset, 6) as $value) {
-            echo "<div class=\"product\"><div class=\"blur-overlay\" onclick=\"redirectToProduct(" . $value['ProductID'] . ")\"><img src=" . $value['ProductThumbnail'] . " alt=\"Intel sok\">";
-            echo "<p>" . $value['ProductName'] . "</p>";
-            echo "<p>" . $value['ProductProperties']['Price'] . "</p>";
-            echo "</div><button class=\"button-overlay\">Ga naar product</button></div>";
-        }
+    //generate html code based on the offset
+    foreach (array_slice($productArray, $indexOffset, 6) as $value) {
+        echo "<div class=\"product\"><div class=\"blur-overlay\" onclick=\"redirectToProduct(" . $value['ProductID'] . ")\"><img src=" . $value['ProductThumbnail'] . " alt=\"Intel sok\">";
+        echo "<p>" . $value['ProductName'] . "</p>";
+        echo "<p>" . $value['ProductProperties']['Price'] . "</p>";
+        echo "</div><button class=\"button-overlay\">Ga naar product</button></div>";
+    }
 }
 
 function getFilterResult()
@@ -37,6 +52,7 @@ function getFilterResult()
     $json_file = file_get_contents('./assets/json/products.json');
     $products = json_decode($json_file, true);
 
+    //on page load or when user doesnt supply correct data
     if(!isset($_GET['colorFilter']) && !isset($_GET['collection']))
     {
         //returns all socks
@@ -47,14 +63,17 @@ function getFilterResult()
         $color = null;
         $collection = null;
 
+        // Check whether colorFilter is empty or not
         if(isset($_GET['colorFilter']) && $_GET['colorFilter'] != '')
             $color = filter_var($_GET['colorFilter'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
+        // Check whether collection is empty or not
         if(isset($_GET['collection']) && $_GET['collection'] != '')
             $collection = filter_var($_GET['collection'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
         $queriedProducts = array();
 
+        //Determine what products will be given back
         if($color != null && $collection != null){
             foreach($products as $value){
                 if($value['ProductProperties']['Color'] == $color && $value['ProductProperties']['Collection'] == $collection){
@@ -81,4 +100,3 @@ function getFilterResult()
         return $queriedProducts;
     }
 }
-?>
--- SunnyNL/product-page.php
+++ SunnyNL/product-page.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sunny | Producten</title>
+    <title>Sunny | Product page</title>
     <link rel="stylesheet" href="assets/css/product-page.css">
     <link rel="stylesheet" href="./assets/css/style.css" type="text/css">
     <script src="./assets/js/product-page.js"></script>
+    <link rel="icon" href="./assets/img/favicon/favicon.png" type="image/png">
+
 </head>
 <body>
     <?php
@@ -16,29 +18,69 @@
         <div class="filter-collumn">
             <form method="get" action="./product-page.php" class="filter-container">
                 <h2 class="headings-filters">Categorie</h2>
-                <input type="submit" name="collection" value="Uni kleur" class="filter-items">
-                <input type="submit" name="collection" value="Gestreept" class="filter-items">
+                <div class="dot-container">
+                <?php if(isset($_GET['collection']) && $_GET['collection'] == "Uni kleur")
+                    echo "&#x2022";
+                 ?>
+                 <input type="submit" name="collection" value="Uni kleur" class="filter-items">
+                </div>
+                <div class="dot-container">
+                <?php if(isset($_GET['collection']) && $_GET['collection'] == "Gestreept")
+                    echo "&#x2022";
+                 ?>
+                 <input type="submit" name="collection" value="Gestreept" class="filter-items">
+                </div>
                 <h2 class="headings-filters">Kleuren</h2>
+                <div class="dot-container">
+                <?php if(isset($_GET['colorFilter']) && $_GET['colorFilter'] == "Groen")
+                    echo "&#x2022";
+                 ?>
                 <input type="submit" name="colorFilter" value="Groen" class="filter-items">
+                </div>
+                <div class="dot-container">
+                <?php if(isset($_GET['colorFilter']) && $_GET['colorFilter'] == "Blauw")
+                    echo "&#x2022";
+                 ?>
                 <input type="submit" name="colorFilter" value="Blauw" class="filter-items">
+                </div>
+                <div class="dot-container">
+                <?php if(isset($_GET['colorFilter']) && $_GET['colorFilter'] == "Roze")
+                    echo "&#x2022";
+                 ?>
                 <input type="submit" name="colorFilter" value="Roze" class="filter-items">
+                </div>
+                <div class="dot-container">
+                <?php if(isset($_GET['colorFilter']) && $_GET['colorFilter'] == "Rood")
+                    echo "&#x2022";
+                 ?>
                 <input type="submit" name="colorFilter" value="Rood" class="filter-items">
+                </div>
+                <div class="dot-container">
+                <?php if(isset($_GET['colorFilter']) && $_GET['colorFilter'] == "Geel")
+                    echo "&#x2022";
+                 ?>
                 <input type="submit" name="colorFilter" value="Geel" class="filter-items">
-                <img src="assets/img/products/actie.png" alt="Sale ad" class="actie-img">
+                </div>
+                
             </form>
+            <?php if(isset($_GET['colorFilter']) || isset($_GET['collection'])){
+                echo "            <form class=\"reset-form\" method=\"get\" action=\"./product-page.php\">
+                <input type=\"submit\" value=\"Reset Filter Set\" class=\"reset-filter-items\">
+            </form>";
+            } ?>
+            <img src="assets/img/products/actie.png" alt="Sale ad" class="actie-img">
         </div>
         <div class="flex-container">
             <?php
-            include('product-page-filter-handler.php');
+            include('./includes/product-page-filter-handler.php');
             generateProductPage();
+            generatePaging();
             ?>
-                    <form method="get" class="paging-nav">
-            <input type="submit" name="page" value="1" class="page-button">
-            <input type="submit" name="page" value="2" class="page-button">
-        </form>
         </div>
     </div>
-    <?php include "./includes/cta.php" ?>
-    <?php include "./includes/footer.php" ?>
+    <?php
+    include "./includes/cta.php";
+    include('./includes/footer.php');
+    ?>
 </body>
 </html>
